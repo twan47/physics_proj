e4cce87f91fe2de39c1b57dbebc46e7fac5d233e
Twan47 <anthonynewell171@gmail.com>
2023-03-22 12:40:33 -0400

    added train force function

--- script.py
+++ script.py
@@ -1,9 +1,14 @@
-# Uncomment this when you reach the "Use the Force" section
-# train_mass = 22680
-# train_acceleration = 10
-# train_distance = 100
-# bomb_mass = 1
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
 
+def f_to_c(f_temp):
+    return (f_temp - 32) * 5/9
+print(f_to_c(47))
 
-# Write your code below: 
-
+def get_force(mass, acceleration):
+    return mass * acceleration
+train_force = get_force(train_mass, train_acceleration)
+print(train_force)
+print
