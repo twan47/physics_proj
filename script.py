e1f4580ac68038739e28fb18f7e617130588fa50
Twan47 <anthonynewell171@gmail.com>
2023-03-22 12:57:55 -0400

    added energy function

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ train_mass = 22680
 train_acceleration = 10
 train_distance = 100
 bomb_mass = 1
+c = 3*10**8
 
 def f_to_c(f_temp):
     return (f_temp - 32) * 5/9
@@ -12,3 +13,12 @@ def get_force(mass, acceleration):
 train_force = get_force(train_mass, train_acceleration)
 print(train_force)
 print("The GE train supplies " + str(train_force) + " Newtons of force.")
+
+def get_energy(mass, c):
+    return mass * c**2
+bomb_energy = get_energy(bomb_mass, c)
+print(bomb_energy)
+print("A 1kg bomb supplies " + str(bomb_energy) + " Joules.")
+
+def get_work(mass, acceleration, distance):
+    
